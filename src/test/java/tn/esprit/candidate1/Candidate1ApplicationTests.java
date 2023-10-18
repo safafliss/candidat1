8435d6e7c5d577a7c5261377615fbb7df5440b66
safafliss <safa.fliss@esprit.tn>
2023-10-18 14:43:51 +0100

    first

---
+++ src/test/java/tn/esprit/candidate1/Candidate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.candidate1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Candidate1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
